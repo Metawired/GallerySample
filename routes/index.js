58f4551993561576ebc2e262adaeddf3bbc8ad7e
Wojciech Trocki <wtrocki@redhat.com>
2016-04-07 23:34:11 +0100

    Health check endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var package = require("../package.json")
+
 /*
  * GET
  */
@@ -9,5 +10,10 @@ router.get('/', function(req, res) {
     res.json({doc:"./doc",version:package.version});
 });
 
+// Health check
+router.get('/sys/info/ping', function(req, res) {
+	res.set('Content-Type', 'text/plain');
+    res.send("\"OK\"");
+});
  
 module.exports = router;
