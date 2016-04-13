37c189c8f7b42138b86890b5a794386017751f95
wtrocki <wtrocki@redhat.com>
2016-04-13 18:53:13 +0100

    Add reference

--- models/galleryModel.js
+++ models/galleryModel.js
@@ -1,6 +1,17 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var gallerySchema = new Schema({	"name" : String,	"type" : String,	"mainImageUrl" : String,	"thumbnails" : Array,	"images" : Array,	"user" : {	 	type: Schema.Types.ObjectId,	 	ref: 'user'	}});
+var gallerySchema = new Schema({
+  "reference": String,
+	"name" : String,
+	"type" : String,
+	"mainImageUrl" : String,
+	"thumbnails" : Array,
+	"images" : Array,
+	"user" : {
+	 	type: Schema.Types.ObjectId,
+	 	ref: 'user'
+	}
+});
 
 module.exports = mongoose.model('gallery', gallerySchema);
